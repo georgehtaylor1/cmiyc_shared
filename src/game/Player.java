4f4dbf8225e60d4655ed1c1f084e062c71c89e5d
George Taylor <george.h.taylor1@gmail.com>
2017-02-22 12:18:35 +0000

    Updated volume functions
    The two new functions calculate the proportion of volume in each ear based on the volume of every player around them. Every player now has a volume attribute that could be modified in accordance with collectables?

--- src/game/Player.java
+++ src/game/Player.java
@@ -8,37 +8,41 @@ import game.util.Position;
 
 public class Player implements Serializable {
 
-    private static final long serialVersionUID = -6117845925839528271L;
+	private static final long serialVersionUID = -6117845925839528271L;
 
-    public String clientID;
+	public String clientID;
 
-    public double speed;
-    public double battery;
+	public double speed;
+	public double battery;
 
-    public Position position;
-    public double direction;
+	public Position position;
+	public double direction;
 
-    public Faction faction;
-    public GameMode mode;
+	public Faction faction;
+	public GameMode mode;
 
-    public PlayerState state;
-    public String dragging;
+	public PlayerState state;
+	public String dragging;
 
-    /**
-     * Create a new player with the specified client ID
-     *
-     * @param _clientID The ID of the client whom the player represents
-     */
-    public Player(String _clientID) {
-        this.clientID = _clientID;
+	public double volume;
 
-        this.speed = 1.5;
-        this.battery = 1;
-        this.direction = 0.0;
-        this.position = new Position();
-        this.faction = GameSettings.Player.defaultFaction;
+	/**
+	 * Create a new player with the specified client ID
+	 *
+	 * @param _clientID
+	 *            The ID of the client whom the player represents
+	 */
+	public Player(String _clientID) {
+		this.clientID = _clientID;
 
-        this.mode = GameSettings.Player.defaultDesiredMode;
-    }
+		this.speed = 1.5;
+		this.battery = 1;
+		this.direction = 0.0;
+		this.position = new Position();
+		this.volume = 1.0;
+		this.faction = GameSettings.Player.defaultFaction;
+
+		this.mode = GameSettings.Player.defaultDesiredMode;
+	}
 
 }
