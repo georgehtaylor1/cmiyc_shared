869426a6ef4699124ec0c029b9e2f107e0acdd58
George Taylor <george.h.taylor1@gmail.com>
2017-02-28 17:57:31 +0000

    Updated to the position class
    Added the function 'at' which compares two points to see if they are the same

--- src/game/util/Position.java
+++ src/game/util/Position.java
@@ -1,18 +1,47 @@
 package game.util;
 
 import game.constants.GameSettings;
+import util.Maths;
 
 /**
- * @author Denis Makula
- * Position Class (this is just an x/y wrapper, which should be translated into Point or Point2D after it has been transfered, unpacked and unserialised)
+ * @author Denis Makula Position Class (this is just an x/y wrapper, which should be translated into Point or Point2D after it has been transfered, unpacked and
+ *         unserialised)
  **/
 public class Position {
 
 	public double x = 0;
 	public double y = 0;
-	
-	public Position() { this( ( GameSettings.Arena.size.width / 2 ), ( GameSettings.Arena.size.height / 2 ) ); }
-	
-	public Position( double _x, double _y ) { this.x = _x;this.y = _y; }
-	
+
+	/**
+	 * Create a new position in the centre of the screen
+	 */
+	public Position() {
+		this((GameSettings.Arena.size.width / 2), (GameSettings.Arena.size.height / 2));
+	}
+
+	/**
+	 * Create a new position at the specified x an y coordinates
+	 * 
+	 * @param _x
+	 *            The x coordinate of the new position
+	 * @param _y
+	 *            The y coordinate of the new position
+	 */
+	public Position(double _x, double _y) {
+		this.x = _x;
+		this.y = _y;
+	}
+
+	/**
+	 * Determine whether this position is the same as the given position, This is a necessary function in order to eliminate floating point errors that could
+	 * occur if equality was used to compare two positions
+	 * 
+	 * @param p
+	 *            The position to compare to
+	 * @return Whehter the position being compared to is the same
+	 */
+	public boolean at(Position p) {
+		return Maths.dist(this, p) < 1.0;
+	}
+
 }
