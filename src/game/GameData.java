ba9e4777855967f44e4eb6c21ca7dd819e96edf0
Denis Makula <emdeevy@gmail.com>
2017-03-21 11:42:23 +0000

    Weird stuff

--- src/game/GameData.java
+++ src/game/GameData.java
@@ -46,43 +46,29 @@ public class GameData {
      */
     public boolean fits(Player _player) {
 
-        if (this.state != GameState.PREGAME) {
-            return false;
-        }
+        if (this.state != GameState.PREGAME) { return false; }
 
         Player player = _player;
 
         // Calculate the limit on the number of players able to connect to the server
         int maxPlayerCount = (this.mode == GameMode.SHORT) ? 3 : 5;
-        int limitPerFaction = ((this.mode == GameMode.SHORT)) ? ((player.faction == Faction.THIEF) ? 1 : 2)
-                : ((player.faction == Faction.THIEF) ? 2 : 3);
+        int limitPerFaction = ((this.mode == GameMode.SHORT)) ? ((player.faction == Faction.THIEF) ? 1 : 2) : ((player.faction == Faction.THIEF) ? 2 : 3);
 
         // Check for space in the game session
-        if (this.players.size() == maxPlayerCount) {
-            return false;
-        } else if (this.mode != player.mode) {
-            return false;
-        } else if (this.players.size() == 0) {
-            return true;
-        } else {
+        if (this.players.size() == maxPlayerCount) { return false; } 
+        else if (this.mode != player.mode) { return false; }
+        else if (this.players.size() == 0) { return true; }
+        else {
             int existingPerFaction = 0;
             ConcurrentHashMap<String, Player> tempPlayers = new ConcurrentHashMap<String, Player>(this.players);
 
+            for( Map.Entry<String, Player> pair : tempPlayers.entrySet() ) {
+            	
+            	Player tempPlayer = pair.getValue();
+    			if( tempPlayer.faction == player.faction ) { existingPerFaction++; }
+
+            }
             
-            Iterator<Entry<String, Player>> i = tempPlayers.entrySet().iterator();
-
-    		while (i.hasNext()) {
-
-    			Map.Entry<String, Player> pair = (Map.Entry<String, Player>) i.next();
-
-    			Player tplayer = pair.getValue();
-    			
-    			if (tplayer.faction == player.faction) {
-                    existingPerFaction++;
-                }
-
-    		}
-
             return (existingPerFaction < limitPerFaction);
         }
 
--- src/game/util/Movement.java
+++ src/game/util/Movement.java
@@ -3,13 +3,16 @@ package game.util;
 public class Movement {
 
 	public String clientID;
+	
 	public Position position;
 	public double direction;
+	public double battery;
 	
-	public Movement( String _clientID, Position _position, double _direction ) {
+	public Movement( String _clientID, Position _position, double _direction, double _battery ) {
 		this.clientID = _clientID;
 		this.position = _position;
 		this.direction = _direction;
+		this.battery = _battery;
 	}
 	
 }
