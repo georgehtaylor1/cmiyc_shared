d9dda12b9e2556cb5272406b0a818085fed6ebd6
Paul Sandbrook <pes534@cs.bham.ac.uk>
2017-03-23 14:29:02 +0000

    Add cameraVision to Colors

--- src/constants/Colors.java
+++ src/constants/Colors.java
@@ -3,13 +3,13 @@ package constants;
 import javafx.scene.paint.Color;
 
 /**
- * @author Denis Makula
- *         Class full of Color constants to be used by the game
+ * @author Denis Makula Class full of Color constants to be used by the game
  **/
 public class Colors {
 
     public static Color fog = Color.rgb(56, 60, 64, 1);
     public static Color flashlight = Color.rgb(255, 200, 0, 0.5);
+    public static Color cameraVision = Color.rgb(200, 200, 200, 0.5);
     public static Color arena = Color.rgb(105, 106, 110, 1);
     public static Color outerArena = Color.rgb(51, 54, 58, 1);
     public static Color playerShadow = Color.rgb(0, 0, 0, 1);
@@ -17,9 +17,9 @@ public class Colors {
     public static Color stuckSecurity = Color.rgb(105, 106, 110, 1);
     public static Color activeThief = Color.rgb(239, 73, 72, 1);
     public static Color treasure = Color.rgb(249, 164, 29, 1);
-    public static Color treasureShadow = Color.rgb(105, 106, 110, 1);
+    public static Color treasureShadow = Color.rgb(90, 92, 100, 1);
     public static Color thiefVision = Color.rgb(50, 50, 50, 0.5);
-    
+
     public static Color lightest = Color.rgb(105, 106, 110, 1);
     public static Color lighter = Color.rgb(56, 60, 64, 1);
     public static Color normal = Color.rgb(51, 54, 58, 1);
@@ -28,7 +28,7 @@ public class Colors {
 
     public static Color green1 = Color.rgb(68, 179, 128, 1);
     public static Color yellow = Color.rgb(249, 164, 29, 1);
-    public static Color red =  Color.rgb(239, 73, 72, 1);
+    public static Color red = Color.rgb(239, 73, 72, 1);
     public static Color blue = Color.rgb(113, 136, 218, 1);
     public static Color black = Color.rgb(0, 0, 0, 1);
     public static Color white = Color.rgb(255, 255, 255, 1);
